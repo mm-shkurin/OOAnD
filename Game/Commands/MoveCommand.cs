1fc60cac1c209f4dc644c2387c516843a5b67b34
brndmgd <brndmgdwastaken@gmail.com>
2026-05-05 21:46:55 +0600

    feat: MoveCommand

---
+++ Game/Commands/MoveCommand.cs
@@ -0,0 +1,12 @@
+using Game.Interfaces;
+using Game.Models;
+
+namespace Game.Commands;
+
+public class MoveCommand(IMovable movingObject) : ICommand
+{
+    public void Execute()
+    {
+        movingObject.Position += movingObject.Speed;
+    }
+}
