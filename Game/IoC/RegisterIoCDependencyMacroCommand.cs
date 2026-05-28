82d740006cacbcf89920e8df033c3852ef8e8296
Egor <brndmgdwastaken@gmail.com>
2026-05-28 14:37:54 +0600

    feat: IoC register MacroCommand implementation
    * feat: IoC register MacroCommand tests
    
    * feat: IoC register MacroCommand implementation

---
+++ Game/IoC/RegisterIoCDependencyMacroCommand.cs
@@ -0,0 +1,17 @@
+using App;
+using Game.Commands;
+using Game.Interfaces;
+
+namespace Game.IoC;
+
+public class RegisterIoCDependencyMacroCommand : ICommand
+{
+    public void Execute()
+    {
+        Ioc.Resolve<ICommand>(
+            "IoC.Register",
+            "Commands.Macro",
+            (object[] args) => new MacroCommand((ICommand[])args[0])
+        ).Execute();
+    }
+}
