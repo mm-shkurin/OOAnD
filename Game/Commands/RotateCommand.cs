47438118191486fa0414fc45aac2674af02c677d
Ivan Shatilov <irshatilov@st.omgtu.ru>
2026-05-28 14:01:47 +0600

    feat: RotateCommand functionality
    * build : add pipline
    
    * fix : name sln
    
    * feat : add icommand
    
    * style : add enter
    
    * feat: Angle tests
    
    * feat: Angle
    
    * feat: RotateCommand tests
    
    * feat: IRotatable interface
    
    * feat: RotateCommand
    
    * fix: declare Angle as nullable in RotateCommandTests.cs
    
    * fix: delete ICommand implementation
    
    * fix: use ICommand from App
    
    ---------
    
    Co-authored-by: mm shkurin <trape3977@g,ail.com>
    Co-authored-by: Egor <brndmgdwastaken@gmail.com>

---
+++ Game/Commands/RotateCommand.cs
@@ -0,0 +1,12 @@
+using App;
+using Game.Interfaces;
+
+namespace Game.Commands;
+
+public class RotateCommand(IRotatable rotatingObject) : ICommand
+{
+    public void Execute()
+    {
+        rotatingObject.Orientation += rotatingObject.AngularSpeed;
+    }
+}
